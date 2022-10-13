df7b9c9c66ad27cb8c5c7e5eddf5f83220493cee
BiniyamTT <biniyamttufa@gmail.com>
2022-10-13 13:28:11 +0300

    task_5

---
+++ 0x00-hello_world/5-prinf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints the phrase using printf
+ * Return: 0  - Successful, Non 0, Errenous
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,"\n);
+	return (0);
+}
