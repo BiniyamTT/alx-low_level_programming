6701c0c91a205c41db236d7792b7e7fa6f93c944
BiniyamTT <biniyamttufa@gmail.com>
2022-10-13 12:48:19 +0300

    Task_4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the required string
+ * Return: 0 if successfully exited, or non zero if failed
+ */
+
+int main(void)
+{
+	char str[] = "Programming is like builiding a multilingual puzzle";
+
+	puts(str);
+	return (0);
+}
