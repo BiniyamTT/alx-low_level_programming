3c337014e3a9439f49d1ed2434861e87e9bf1703
BiniyamTT <biniyamttufa@gmail.com>
2022-10-14 13:28:17 +0300

    Mod-gitignore_and-Task_2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints a-z in lowercase
+ * Return: 0 Successful else Failed
+ */
+
+int main(void)
+{
+	int i = 97;
+
+	for (i; i < 123; i++)
+		putchar((char)i);
+	putchar('\n');
+	return (0);
+}
