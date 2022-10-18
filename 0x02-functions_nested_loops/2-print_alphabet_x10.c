1efa92d021d47bbffc55dfd0cde767b867363c82
BiniyamTT <biniyamttufa@gmail.com>
2022-10-18 13:18:19 +0300

    Tasl2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet, in lowercase,
+ * 10 times followed by a new line
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+        int c = 1;
+
+	while(c <= 10)
+	{
+		char i = 'a';
+
+        	for (; i <= 'z'; i++)
+                	_putchar(i);
+        	_putchar('\n');
+		c++;
+	}
+}
