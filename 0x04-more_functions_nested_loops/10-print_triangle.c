e66d4b7acbddbbfec01f970f0e818b321f87b4cb
BiniyamTT <biniyamttufa@gmail.com>
2022-10-20 13:49:18 +0300

    Task10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_triangle - check the code
+ * @size: integer
+ */
+
+void print_triangle(int size)
+{
+	int row, col;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (row = 1; row <= size; row++)
+		{
+			for (col = size ; col > 0; col--)
+			{
+				if (col <= row)
+					_putchar ('#');
+				else
+					_putchar ('.');
+			}
+			_putchar('\n');
+		}
+	}
+}
