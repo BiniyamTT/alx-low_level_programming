84f37bf33ac1e81e7754995039e7a061f7ad5fb5
BiniyamTT <biniyamttufa@gmail.com>
2022-10-20 13:15:01 +0300

    TaskFizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i < 101; i++)
+	for (i = 1; i < 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 != 0)
 			printf("Fizz ");
@@ -22,6 +22,6 @@ int main(void)
 		else
 			printf("%d ", i);
 	}
-	printf("\n");
+	printf("Buzz\n");
 	return (0);
 }
