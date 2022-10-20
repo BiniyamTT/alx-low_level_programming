fc9ce20ac8351c7f5012155ffd276a61338ecabf
BiniyamTT <biniyamttufa@gmail.com>
2022-10-20 11:06:24 +0300

    Task1

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - check the code.
+ *
+ * Return: 1 - Uppercase 0 - Otherwise.
+ */
+
+int _isupper(int c);
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
